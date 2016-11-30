7044f82c14e7aed0dd03404d29aea692cb893363
Hebert Porto <hebertporto@gmail.com>
2016-11-30 21:54:23 -0200

    agora o jwt esta validando

--- config/passport.js
+++ config/passport.js
@@ -6,26 +6,21 @@ var config      = require('./config.js');
 module.exports = function (passport) {
 	var  opts = {};
 	opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
- console.log(ExtractJwt.fromAuthHeader());
+
 	opts.secretOrKey  = config.secret;
 
 	passport.use(new JwtStrategy(opts, function(jwt_payload, done){
 
-		console.log("entrou na estrategia");
 
-		User.findOne({_id: jwt_payload.id}, function(err, user){
-			console.log("AHHH");
+		User.findOne({_id: jwt_payload._id}, function(err, user){
+			
 			if (err) return done(err, false);
 
-			if(user){
-				console.log("user", user);
+			if(user)
 				done(null, user)
-			}
 			else
-			{
-				console.log("fail user");
 				done(null, false);
-			}
+			
 		});
 
 	}));
