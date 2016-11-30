d4725a81e2abadcf2e57b36f0a429f4f122a938c
Hebert Porto <hebertporto@gmail.com>
2016-11-29 22:30:33 -0200

    configurando jwt

--- config/passport.js
+++ config/passport.js
@@ -5,12 +5,13 @@ var config      = require('./config.js');
 
 module.exports = function (passport) {
 	var  opts = {};
-	opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
+	opts.jwtFromRequest = ExtractJwt.fromAuthHeader('authorization');
 
-	opts.secretOrKey = config.secret;
-	
+	opts.secretOrKey  = config.secret;
+
 	passport.use(new JwtStrategy(opts, function(jwt_payload, done){
-	console.log("opts jwt id|",  jwt_payload.id);
+	
+		console.log("entrou na estrategia");
 
 		User.findOne({_id: jwt_payload.id}, function(err, user){
 			if (err) return done(err, false);
