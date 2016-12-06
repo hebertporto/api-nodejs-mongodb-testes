11bb1e127338d3d715728b31f38ad060a07d4128
Hebert Porto <hebertporto@gmail.com>
2016-12-06 21:38:44 -0200

    removendo seguranca das rotas para testar interceptors no frontend

--- routes.js
+++ routes.js
@@ -4,7 +4,8 @@ require('./config/passport.js')(passport);
 
 
 module.exports = function (app) {
-        app.use('/products',passport.authenticate('jwt', {session: false}), require('./lib/products'));
+        // app.use('/products',passport.authenticate('jwt', {session: false}), require('./lib/products'));
+        app.use('/products', require('./lib/products'));
         app.use('/users', require('./lib/users'));
 
 };
